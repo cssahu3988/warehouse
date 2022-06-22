a2f337c459d7d49e8ee947a54dca250ae2bb6142
chandra sekhar <chandrasekhar.s@mantralabsglobal.com>
2022-06-22 17:28:04 +0530

    c2

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -12,6 +12,9 @@ public class Main {
         server.get("/home/chandra", false, (req, res) -> {
             res.send("{Hi from server home chandra}");
         });
+        server.post("/",false,(req,res)->{
+            res.send("upload done");
+        });
         server.listen(8080);
         /*String s = "/";
         String s1 = s.substring(0,s.lastIndexOf("/"));
--- src/main/java/com/example/demo/Server.java
+++ src/main/java/com/example/demo/Server.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -13,7 +15,7 @@ public class Server {
     private BufferedInputStream bufferedInputStream = null;
     private OutputStream outputStream = null;
     private Map<String,Object> GET_MAP = new HashMap<>();
-
+    private Map<String,Object> POST_MAP = new HashMap<>();
     public Server(){
 
     }
@@ -22,13 +24,12 @@ public class Server {
         init();
     }
     private void init() {
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             System.out.println("listening on :" + serverSocket.getInetAddress() + "/" + PORT);
             while (true) {
                 try {
-                    Socket socket;
-                    socket = serverSocket.accept();
+                    Socket socket = serverSocket.accept();
                     executorService.submit(new Runnable() {
                         @Override
                         public void run() {
@@ -47,17 +48,21 @@ public class Server {
         try {
             System.out.println("Accepted connection..." + socket);
             InputStream inputStream = socket.getInputStream();
-            BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+            byte[] b = getBytesFromInputStream(inputStream);
+            System.out.println(new String(b));
+            /*BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
             String input;
             StringBuilder total= new StringBuilder();
             ArrayList<String> request = new ArrayList<>();
-            while (!Objects.equals(input = in.readLine(), "")){
-                if (input==null){
-                    break;
-                }
+            while ((input = in.readLine())!=null){
                 request.add(input);
                 total.append(input);
                 System.out.println(""+input);
+                if (input.contains("Content-length")){
+                    int length = Integer.parseInt(input.split(":")[1].trim());
+
+                    byte[] content = new byte[length];
+                }
             }
             if (total.toString().isEmpty()){
                 System.out.println("hand-shake");
@@ -87,9 +92,86 @@ public class Server {
                         }
                     }
                     break;
-            }
+                case "POST":
+                    while (true){
+                        Object o = POST_MAP.get(path);
+                        if (o!=null){
+                            Request req = new Request(total.toString());
+                            Object[] oo = (Object[]) o;
+                            if (!(boolean)oo[0]){
+                                Response res = new Response(socket);
+                                ((post)oo[1]).onPost(req,res);
+                            }
+                            else{
+                                //todo code
+                            }
+                            break;
+                        }
+                        path = path.substring(0,path.lastIndexOf("/"));
+                        if (path.isEmpty()){
+                            path = "/";
+                        }
+                    }
+                    break;
+            }*/
         } catch (Exception e) {
             System.out.printf(""+e);
+        }
+    }
+    public static StringBuilder getString(InputStream in) throws IOException {
+        int length = 1024*10;
+        byte[] messageByte = new byte[length];
+        boolean end = false;
+        StringBuilder dataString = new StringBuilder(length);
+        int totalBytesRead = 0;
+        while(!end) {
+            int currentBytesRead = in.read(messageByte);
+            totalBytesRead = currentBytesRead + totalBytesRead;
+            if(totalBytesRead <= length) {
+                dataString
+                        .append(new String(messageByte, 0, currentBytesRead, StandardCharsets.UTF_8));
+            } else {
+                dataString
+                        .append(new String(messageByte, 0, length - totalBytesRead + currentBytesRead,
+                                StandardCharsets.UTF_8));
+            }
+            if(dataString.length()>=length) {
+                end = true;
+            }
+        }
+        return dataString;
+    }
+    public static byte[] getBytesFromInputStream(InputStream is) throws IOException {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        byte[] buffer = new byte[0xFFFF];
+        for (int len = is.read(buffer); len != -1; len = is.read(buffer)) {
+            os.write(buffer, 0, len);
+        }
+        return os.toByteArray();
+    }
+    public static byte[] readAllBytes(InputStream inputStream) throws IOException {
+        final int bufLen = 4 * 0x400; // 4KB
+        byte[] buf = new byte[bufLen];
+        int readLen;
+        IOException exception = null;
+
+        try {
+            try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+                while ((readLen = inputStream.read(buf, 0, bufLen)) != -1)
+                    outputStream.write(buf, 0, readLen);
+
+                return outputStream.toByteArray();
+            }
+        } catch (IOException e) {
+            exception = e;
+            throw e;
+        } finally {
+            if (exception == null) inputStream.close();
+            else try {
+                inputStream.close();
+            } catch (IOException e) {
+                exception.addSuppressed(e);
+            }
         }
     }
     public void get(String path, boolean isAuth, get g) {
@@ -99,7 +181,13 @@ public class Server {
     interface get{
         void onGet(Request req, Response res);
     }
-
+    public void post(String path, boolean isAuth, post p) {
+        Object[] ar = {isAuth, p};
+        POST_MAP.put(path,ar);
+    }
+    interface post{
+        void onPost(Request req, Response res);
+    }
     public static class Request{
         Socket socket;
         String s;
